91e79b4ce44e110d8c3ec82e28714234de39ca1f
Jeff Hykin <jeff.hykin@gmail.com>
2025-09-16 19:57:30 -0500

    add export

--- main/main.js
+++ main/main.js
@@ -3,6 +3,7 @@ import openAlex from "./plugins/openAlex/3_standard_api.js"
 import crossRef from "./plugins/crossRef/3_standard_api.js"
 import googleScholar from "./plugins/googleScholar/3_standard_api.js"
 
+export { ReferenceSystem }
 export const { Reference, search, loadReferences, getReferences, } = ReferenceSystem({
     plugins: {
         // NOTE: order matters
